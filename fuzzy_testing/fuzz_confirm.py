20f6e63dc25d36f47889a5f748718c8c0309ad3f
GregMcC5 <gregmcc@umich.edu>
2022-12-13 10:35:31 -0500

    add fuzz_confirm directory

---
+++ fuzzy_testing/fuzz_confirm.py
@@ -0,0 +1,44 @@
+import metadata_utils as mu
+from fuzzywuzzy import fuzz
+
+THRESHOLD = 50
+
+tpg_content = mu.read_file("tpg_content_full.json") + mu.read_file("tpg_content_full2.json")
+passed_records = mu.read_file("passed.csv")
+
+fail_counter = 0
+
+# print([x["dlps_id"] for x in tpg_content], "\n,\n")
+# print([x["dlps_id"] for x in tpg_content])
+
+
+tpg_ids = [x["dlps_id"] for x in tpg_content]
+
+#comparing it with current OCR contetn
+for record in passed_records[1:]:
+    if record[1] in tpg_ids:
+        record_scores = []
+        match = [y for y in tpg_content if y["dlps_id"] == record[1]]
+        if len(match) == 1:
+            match = match[0]
+            print("match:", match)
+        elif len(match) > 1:
+            print("\n\nsomething bad happened\n\n")
+            print(match)
+            match = None
+        if match:
+            scores = []
+            if len(match.keys()) > 2:
+                ocr_content = [val for key,val in match.items() if "tpg" in key]
+                for ocr in ocr_content:
+                    scores.append(fuzz.partial_ratio(record[3].lower().strip(), ocr.lower()))
+                top_score = max(scores)
+                print(top_score)
+
+
+        # tpg_ocr = [[val if 'tpg' in key else None for key,val in x.items()] for x in tpg_content if x["dlps_id"] == record[1]]
+print(fail_counter)
+print("done")
+
+
+
