632f5c3d9217360a38c2067a0ff8d74ebbc59db8
GregMcC5 <gregmcc@umich.edu>
2022-12-14 11:18:14 -0500

    tweak fuzzy string matching

--- fuzzy_testing/fuzz_confirm.py
+++ fuzzy_testing/fuzz_confirm.py
@@ -28,19 +28,22 @@ for record in passed_records[1:]:
             if len(match.keys()) > 2:
                 #ocr_content = {key : val for key,val in match.items() if "tpg" in key}
                 for key, val in {key : val for key,val in match.items() if "tpg" in key}.items():
-                    scores.append([fuzz.partial_ratio(record[3].lower().strip(), val.lower()), key, val])
+                    scores.append([fuzz.partial_ratio(record[3], val), key, val])
                 top_score = sorted(scores,key=lambda x:x[0], reverse=True)[0]
-                ocr_sample = top_score[2][:40].replace("\n", "")
-                print(top_score)
+                if record[1] == "ABK8873.0006.001":
+                    print(top_score, record[3])
                 with open("fuzz_output.txt", 'a') as file:
-                    file.write(f"{record[3]}\nfuzz score: {top_score[0]}, title page index:? {key}\n\n")
+                    file.write(f"{record[3]}\nfuzz score: {top_score[0]}, title page index:? {key}\nid: {record[1]}\n\n")
 
 
 print(fail_counter)
+
+print("fuzz_test example:", fuzz.partial_ratio("American history,", "\nI LLU STRATED\nWITIH NUMEROUS MAPS AND ENGRAVINGS.\nREVO)iT oiI     THO E c[ ) GLO)N IES.\nN~~~~ )2~or~~~:.\n2l\n~odu; o    &inc\n\n\n\n"))
+
 print("done")
 
 
-# First Round of Fuzz Results:
+# First Round of Fuzz Results (using .strip() and .lower()):
 #   - 100   : 101
 #   - 99-90 : 3075
 #   - 89-80 : 2618
@@ -52,3 +55,16 @@ print("done")
 #   - 29-20 : 41
 #   - 19-10 : 15
 #   -  9-0  : 20
+
+# Second Round of Fuzz Results (using neither .strip() or .lower()):
+#   - 100   : 1
+#   - 99-90 : 46
+#   - 89-80 : 57
+#   - 79-70 : 73
+#   - 69-60 : 124
+#   - 59-50 : 240
+#   - 49-40 : 1038
+#   - 39-30 : 1320
+#   - 29-20 : 2557
+#   - 19-10 : 2688
+#   -  9-0  : 51
