d16d828468dd8fa491044a4f949ca51304a2d1f5
GregMcC5 <gregmcc@umich.edu>
2022-12-19 08:32:25 -0500

    raise threshold

--- fuzzy_testing/fuzz_confirm.py
+++ fuzzy_testing/fuzz_confirm.py
@@ -1,14 +1,20 @@
 import metadata_utils as mu
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
+import pytesseract as pyt
+import os.path
+import os
 
-THRESHOLD = 50
+THRESHOLD = 75
 
 tpg_content = mu.read_file("tpg_content_full.json") + mu.read_file("tpg_content_full2.json")
 passed_records = mu.read_file("passed.csv")
 
 fail_counter = 0
 
+passed_first = []
+full_paths = []
+re_ocr_success = []
 
 tpg_ids = [x["dlps_id"] for x in tpg_content]
 
@@ -30,7 +36,7 @@ for record in passed_records[1:]:
                 page = selection.split("||")[0]
                 ocr = selection.split("||")[1]
                 # #-Doing some testing
-                if record[1] == "ABK8873.0006.001":
+                if record[1] == "AAF1187.1843.001":
                     print(f'''
                     score: {score}
                     title: {record[3]}
@@ -38,11 +44,29 @@ for record in passed_records[1:]:
                     ocr : {ocr}
                     ''')
                 # #-appending to a text file
-                # with open("fuzz_output.txt", 'a') as file:
-                #     file.write(f"{record[3]}\nfuzz score: {top_score[0]}, title page index:? {key}\nid: {record[1]}\n\n")
+                with open("fuzz_output.txt", 'a') as file:
+                    file.write(f"{record[3]}\nfuzz score: {score}, title page index:? {page}\nid: {record[1]}\n\n")
+                # #-checking score, appending
+                # if score > THRESHOLD:
+                #     passed_first.append(record)
+                # elif score < THRESHOLD:
+                #     fail_counter += 1
+                    # #-write filenames
+                    # root = record[1]
+                    # path = f"/n1/obj/{root[0]}/{root[1]}/{root[2]}/{root}/{page.strip('tpg - ')}.tif"
+                    # full_paths.append(path)
+                    # #-check for filepath, do re-OCR
+                    # img_files = os.listdir("moa_images")
+                    # if path in img_files:
+                    #     if process.extractOne(record[3],[pyt.image_to_string(path)], scorer=fuzz.partial_ratio)[1] > THRESHOLD:
+                    #         re_ocr_success.append(record)
+                    #     else:
+                    #         print("still a problem")
 
 
-print(fail_counter)
+
+
+#print(fail_counter)
 
 # print("fuzz_test example:", fuzz.partial_ratio("American history,", "\nI LLU STRATED\nWITIH NUMEROUS MAPS AND ENGRAVINGS.\nREVO)iT oiI     THO E c[ ) GLO)N IES.\nN~~~~ )2~or~~~:.\n2l\n~odu; o    &inc\n\n\n\n"))
 
@@ -111,3 +135,33 @@ print("done")
 # Score without .lower() = 16
 # Score with .lower() = 92
 #
+#
+# Fourth Round of Fuzz Results using process.extractOne() with processor=partial_ratio
+#   - 100   : 1636
+#   - 99-90 : 3792
+#   - 89-80 : 1102
+#   - 79-70 : 640
+#   - 69-60 : 413
+#   - 59-50 : 256
+#   - 49-40 : 252
+#   - 39-30 : 111
+#   - 29-20 : 49
+#   - 19-10 : 5
+#   -  9-0  : 2
+#
+# I'm thinking maybe a threshold of 75?
+#
+#
+#
+# Fith Round of Fuzz Results using process.extractOne() with processor=token_sort_ratio
+#   - 100   : 0
+#   - 99-90 : 35
+#   - 89-80 : 283
+#   - 79-70 : 603
+#   - 69-60 : 869
+#   - 59-50 : 1115
+#   - 49-40 : 1255
+#   - 39-30 : 1370
+#   - 29-20 : 1369
+#   - 19-10 : 1002
+#   -  9-0  : 294
